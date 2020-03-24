d153f9406256ea890938d8aaa7047b4a2e2b96bd
Juan Martín Pal <juanmartinpal@gmail.com>
2020-03-24 15:56:15 -0300

    -Pasado el datawranging de do a r

--- code/prepare.R
+++ code/prepare.R
@@ -1,21 +1,71 @@
 #This script:
-#Transforms data into r format
+#Datawrangling
 
-rm(list=ls())
-
-
+library(here)
 library(tidyverse)
 library(haven)
 library(ggplot2)
 library(openxlsx)
-library(readxl)
 
-wd <- "C:/Users/juanc/Google Drive/research/aprender"
+base_56anio_alum_dir_2016 <- read_dta("C:/research/aprender/data/raw/base_6grado_alum_dir_2016.dta")
+#base_56anio_alum_dir_2016 <- read_dta("raw/base_56anio_alum_dir_2016.dta")
+#base_56anio_alum_dir_2017 <- read_dta(here("data","raw","base_56anio_alum_dir_2017.dta"))
 
-setwd(wd)
 
-base_3grado_alum_dir <- read_dta("data/raw/base_3grado_alum_dir.dta")
 
+#Dummy women
+base <- base_56anio_alum_dir_2016 %>%
+	mutate(women = case_when(Ap2 == 1 ~ 0,
+	                         Ap2 == 2 ~ 1))
+
+##Women per class
+#Reference variables:
+  #CUEANEXO: school id
+  #seccion: class id
+base <- base %>%
+  group_by(CUEANEXO,seccion) %>%
+  mutate(totwomen=sum(women, na.rm=TRUE)) %>%
+  mutate(ltotwomen=log(totwomen))
+base <- base[!is.na(base$women),]
+
+
+#school, class and student id  
+base <- base %>%
+  mutate(idschool = as_factor(CUEANEXO))
+base$idschool <- as.numeric(base$idschool)
+
+base <- base %>%
+  mutate(idclass = as_factor(seccion))
+base$idclass <- as.numeric(base$idclass)
+
+base <- base %>%
+ group_by(idclass) %>%
+ mutate(idstudent = row_number())
+
+
+base <- base %>%
+  arrange(idschool,idclass,idstudent) %>%
+  select(idschool,idclass,idstudent,everything())
+
+#Keep schools with more than one class
+base <- base %>%
+  mutate(aux_classes = case_when(idstudent == 1 ~ 1)) %>%
+  group_by(idschool) %>%
+  mutate(totclasses = sum(aux_classes, na.rm=TRUE))
+base <- base[(base$totclasses>1 & !is.na(base$totclasses)),]
+
+#Class size
+base <- base %>%
+  group_by(idclass) %>%
+  mutate(class_size = max(idstudent))
+
+#Average score of peers
+base <- base %>%
+  group_by(idclass) %>%
+  mutate(peers_score_m = sum(mpuntaje_std)) %>%
+  mutate(peers_score_m = (peers_score_m - mpuntaje_std)/(class_size - 1)) %>%
+  mutate(peers_score_l = sum(lpuntaje_std)) %>%
+  mutate(peers_score_l = (peers_score_l - lpuntaje_std)/(class_size - 1))
 
 
 
---
+++ code/stats.do
@@ -0,0 +1,67 @@
+******************************
+* Estimation of peer effects *
+******************************
+
+set more off
+clear all
+
+cd "C:\Users\juanc\Google Drive\research\aprender"
+
+local data = "C:\research\aprender\data\raw"
+
+
+use "`data'/base_6grado_alum_dir_2016", clear
+
+*dummy women
+gen 	women = 0 if Ap2 == 1
+replace women = 1 if Ap2 == 2
+
+**Women per class
+*Reference variables
+*CUEANEXO: school id
+*seccion: class id
+bysort CUEANEXO seccion: egen totwomen = sum(women)
+gen ltotwomen = log(totwomen)
+keep if women != .
+
+
+encode CUEANEXO, gen(idschool)
+encode seccion, gen(idclass)
+
+bysort idclass: gen idstudent = _n
+
+order idschool idclass idstudent
+sort  idschool idclass idstudent
+
+
+*Keep schools with more than one class
+gen aux_classes = 1 if idstudent == 1
+bysort idschool: egen totclasses = sum(aux_classes)
+
+keep if totclasses > 1 & totclasses != .
+
+*Class size
+bysort idclass: gen class_size = idstudent[_N]
+
+*Average score of peers
+bysort idclass: gen peers_score_m = sum(mpuntaje_std)
+replace peers_score_m = (peers_score_m - mpuntaje_std) / (class_size-1)
+gen lpeers_score_m = log(peers_score_m)
+
+bysort idclass: gen peers_score_l = sum(lpuntaje_std)
+replace peers_score_l = (peers_score_l - lpuntaje_std) / (class_size-1)
+gen lpeers_score_l = log(peers_score_l)
+
+*Average characteristics of peers
+local varlist = "edu_madre edu_padre trabaja anios_jardin repitio apoyo buena_relacion"
+
+foreach var of local varlist {
+	bysort idclass: egen peer_`var' = sum(`var')
+	replace peer_`var' = (peer_`var' - `var') / (class_size-1)
+}
+
+
+*FE reg
+* y_i = a + b1 \bar{Y}_{-i} + b2 x_i + b3 \bar{x}_{-i} + e_i
+areg log_lpuntaje lpeers_score_l peer_* ltotwomen sexo class_size auh edu_madre edu_padre trabaja anios_jardin repitio apoyo buena_relacion privado urbano nbi, abs(idschool) vce(cluster idclass)
+areg log_mpuntaje lpeers_score_m peer_* ltotwomen sexo class_size auh edu_madre edu_padre trabaja anios_jardin repitio apoyo buena_relacion privado urbano nbi, abs(idschool) vce(cluster idclass)
