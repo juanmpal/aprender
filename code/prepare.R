b8575f17e3ff0e647fb19cfed34db395cb8c1ec4
Juan Martín Pal <juanmartinpal@gmail.com>
2020-04-30 13:14:22 -0300

    included secondary students

--- code/prepare.R
+++ code/prepare.R
@@ -9,108 +9,133 @@ library(janitor) #examining and cleaning data
 
 data <- "C:/research/aprender/data/"
 
-base_raw <- read_dta(paste0(data,"raw/base_6grado_alum_dir_2016.dta"))
 
+base_list <- c("6grado","56anio")
 
-#Dummy women
-base <- base_raw %>%
-	mutate(women = case_when(Ap2 == 1 ~ 0,
-	                         Ap2 == 2 ~ 1))
-
-##Women per class
-#Reference variables:
-  #CUEANEXO: school id
-  #seccion: class id
-base <- base %>%
-  group_by(CUEANEXO,seccion) %>%
-  mutate(totwomen=sum(women, na.rm=TRUE)) %>%
-  mutate(ltotwomen=log(totwomen))
-base <- base[!is.na(base$women),]
-
-
-#school, class and student id  
-base <- base %>%
-  mutate(idschool = as_factor(CUEANEXO))
-base$idschool <- as.numeric(base$idschool)
-
-base <- base %>%
-  mutate(idclass = as_factor(seccion))
-base$idclass <- as.numeric(base$idclass)
-
-base <- base %>%
- group_by(idclass) %>%
- mutate(idstudent = row_number())
-
-#Province code to numeric
-base <- base %>%
-  mutate(region = as_factor(cod_provincia))
-base$region <- as.numeric(base$region)
-
-#Sorting
-base <- base %>%
-  arrange(idschool,idclass,idstudent) %>%
-  select(idschool,idclass,idstudent,region,everything())
-
-#Keep schools with more than one class
-base <- base %>%
-  mutate(aux_classes = case_when(idstudent == 1 ~ 1)) %>%
-  group_by(idschool) %>%
-  mutate(totclasses = sum(aux_classes, na.rm=TRUE))
-base <- base[(base$totclasses>1 & !is.na(base$totclasses)),]
-
-#Class size
-base <- base %>%
-  group_by(idclass) %>%
-  mutate(class_size = max(idstudent))
-
-#Average score of peers
-base <- base %>%
-  group_by(idclass) %>%
-  mutate(peers_score_m = sum(mpuntaje_std)) %>%
-  mutate(peers_score_m = (peers_score_m - mpuntaje_std)/(class_size - 1)) %>%
-  mutate(peers_score_l = sum(lpuntaje_std)) %>%
-  mutate(peers_score_l = (peers_score_l - lpuntaje_std)/(class_size - 1))
-
-
-
-#----------------------------------#
-# Average characteristics of peers #
-#----------------------------------#
-
-peer_vars <- c("women","edu_madre", "edu_padre", "trabaja", "anios_jardin",
-			   "repitio", "apoyo", "buena_relacion","auh")
-
-#Keep variables of interest
-base <- base %>%
-	select(idschool,idclass,idstudent,region,privado,urbano,nbi,class_size,
-		   mpuntaje_std, lpuntaje_std, peers_score_m,peers_score_l,
-		   !!!peer_vars)
-
-
-#Function that generates peer average of var (not including own value)
-peer_var_gen <- function(data, group_var, peer_var_list) {
-
-	group_var <- enquo(group_var)
-	data <- data %>%
-		group_by(!!group_var)
-	
-	for (var in peer_var_list) {
-		peer_var <- paste0("peer_",var)
-		peer_var <- sym(peer_var)
-		var <- sym(var)
-		data <- data %>%
- 			mutate(!!peer_var := sum(!!var, na.rm=TRUE)) %>%
- 			mutate(!!peer_var := (!!peer_var - !!var)/(class_size-1))
- 	}
-
- 	data <- data %>%
- 		ungroup()
-
-}	
 
 system.time(
-base <- peer_var_gen(base, idclass, peer_vars)
+
+for (eachbase in base_list) {
+
+
+  base_raw <- read_dta(paste0(data,"raw/base_",eachbase,"_alum_dir_2016.dta"))
+
+  #Dummy women
+  base <- base_raw %>%
+    mutate(women = case_when(Ap2 == 1 ~ 0,
+                             Ap2 == 2 ~ 1))
+  
+
+  if (eachbase == "56anio") {
+
+    base <- base %>%
+      mutate(repitio = case_when(repitio_prim == 1 ~ 1,
+                                 repitio_sec == 1  ~ 1,
+                                 repitio_prim == 0 & repitio_sec == 0 ~ 0))
+    
+  }
+
+
+
+  ##Women per class
+  #Reference variables:
+    #CUEANEXO: school id
+    #seccion: class id
+  base <- base %>%
+    group_by(CUEANEXO,seccion) %>%
+    mutate(totwomen=sum(women, na.rm=TRUE)) %>%
+    mutate(ltotwomen=log(totwomen))
+  base <- base[!is.na(base$women),]
+  
+  
+  #school, class and student id  
+  base <- base %>%
+    mutate(idschool = as_factor(CUEANEXO))
+  base$idschool <- as.numeric(base$idschool)
+  
+  base <- base %>%
+    mutate(idclass = as_factor(seccion))
+  base$idclass <- as.numeric(base$idclass)
+  
+  base <- base %>%
+   group_by(idclass) %>%
+   mutate(idstudent = row_number())
+  
+  #Province code to numeric
+  base <- base %>%
+    mutate(region = as_factor(cod_provincia))
+  base$region <- as.numeric(base$region)
+  
+  #Sorting
+  base <- base %>%
+    arrange(idschool,idclass,idstudent) %>%
+    select(idschool,idclass,idstudent,region,everything())
+  
+  #Keep schools with more than one class
+  base <- base %>%
+    mutate(aux_classes = case_when(idstudent == 1 ~ 1)) %>%
+    group_by(idschool) %>%
+    mutate(totclasses = sum(aux_classes, na.rm=TRUE))
+  base <- base[(base$totclasses>1 & !is.na(base$totclasses)),]
+  
+  #Class size
+  base <- base %>%
+    group_by(idclass) %>%
+    mutate(class_size = max(idstudent))
+  
+  #Average score of peers
+  base <- base %>%
+    group_by(idclass) %>%
+    mutate(peers_score_m = sum(mpuntaje_std)) %>%
+    mutate(peers_score_m = (peers_score_m - mpuntaje_std)/(class_size - 1)) %>%
+    mutate(peers_score_l = sum(lpuntaje_std)) %>%
+    mutate(peers_score_l = (peers_score_l - lpuntaje_std)/(class_size - 1))
+  
+  
+  
+  #----------------------------------#
+  # Average characteristics of peers #
+  #----------------------------------#
+  
+  peer_vars <- c("women","edu_madre", "edu_padre", "trabaja", "anios_jardin",
+           "repitio", "apoyo", "buena_relacion","auh")
+  
+  #Keep variables of interest
+  base <- base %>%
+    select(idschool,idclass,idstudent,region,privado,urbano,nbi,class_size,
+         mpuntaje_std, lpuntaje_std, peers_score_m,peers_score_l,
+         !!!peer_vars)
+  
+  
+  #Function that generates peer average of var (not including own value)
+  peer_var_gen <- function(data, group_var, peer_var_list) {
+  
+    group_var <- enquo(group_var)
+    data <- data %>%
+      group_by(!!group_var)
+    
+    for (var in peer_var_list) {
+      peer_var <- paste0("peer_",var)
+      peer_var <- sym(peer_var)
+      var <- sym(var)
+      data <- data %>%
+        mutate(!!peer_var := sum(!!var, na.rm=TRUE)) %>%
+        mutate(!!peer_var := (!!peer_var - !!var)/(class_size-1))
+    }
+  
+    data <- data %>%
+      ungroup()
+  
+  } 
+  
+  base <- peer_var_gen(base, idclass, peer_vars)
+  
+  
+  write.csv(base,paste0(data,"analytical/base_",eachbase,"_2016.csv"))
+
+
+}
+
 )
 
-write.csv(base,paste0(data,"analytical/base_6grado_2016.csv"))
 
--- code/prepare.do
+++ code/prepare.do
@@ -10,83 +10,104 @@ cd "C:\Users\juanc\Google Drive\research\aprender"
 local data = "C:\research\aprender\data\"
 
 
-use "`data'/raw/base_6grado_alum_dir_2016", clear
-
-*dummy women
-gen 	women = 0 if Ap2 == 1
-replace women = 1 if Ap2 == 2
-
-**Women per class
-*Reference variables
-*CUEANEXO: school id
-*seccion: class id
-bysort CUEANEXO seccion: egen totwomen = sum(women)
-gen ltotwomen = log(totwomen)
-keep if women != .
+local base_list = "6grado 56anio"
 
 
-*IDs
-encode CUEANEXO, gen(idschool)
-encode seccion, gen(idclass)
-
-bysort idclass: gen idstudent = _n
-
-rename region region2
-encode cod_provincia, gen(region)
-
-order idschool idclass idstudent region
-sort  idschool idclass idstudent
+foreach eachbase of local base_list {
 
 
-*Keep schools with more than one class
-gen aux_classes = 1 if idstudent == 1
-bysort idschool: egen totclasses = sum(aux_classes)
-keep if totclasses > 1 & totclasses != .
+	use "`data'/raw/base_`eachbase'_alum_dir_2016", clear
+
+	if "`eachbase'" != "6grado" {
+
+		gen repitio = .
+		replace repitio = 0 if repitio_prim == 0 & repitio_sec == 0
+		replace repitio = 1 if repitio_prim == 1 | repitio_sec == 1
+
+		drop repitio_prim repitio_sec
+
+	}
 
 
-*Ordered class per school
-bysort idschool: gen idclass2 = sum(aux_classes)
+	*dummy women
+	gen 	women = 0 if Ap2 == 1
+	replace women = 1 if Ap2 == 2
 
-*Class size
-bysort idclass: egen class_size = max(idstudent)
-
-
-
-*Average score of peers
-bysort idclass: egen peers_score_m = sum(mpuntaje_std)
-replace peers_score_m = (peers_score_m - mpuntaje_std) / (class_size-1)
-gen lpeers_score_m = log(peers_score_m)
-
-bysort idclass: egen peers_score_l = sum(lpuntaje_std)
-replace peers_score_l = (peers_score_l - lpuntaje_std) / (class_size-1)
-gen lpeers_score_l = log(peers_score_l)
+	**Women per class
+	*Reference variables
+	*CUEANEXO: school id
+	*seccion: class id
+	bysort CUEANEXO seccion: egen totwomen = sum(women)
+	gen ltotwomen = log(totwomen)
+	keep if women != .
 
 
+	*IDs
+	encode CUEANEXO, gen(idschool)
+	encode seccion, gen(idclass)
 
-*Average characteristics of peers
-local varlist = "women edu_madre edu_padre trabaja anios_jardin repitio apoyo"
-local varlist = "`varlist' buena_relacion auh"
+	bysort idclass: gen idstudent = _n
 
-foreach var of local varlist {
-	bysort idclass: egen peer_`var' = sum(`var')
-	replace peer_`var' = (peer_`var' - `var') / (class_size-1)
+	rename region region2
+	encode cod_provincia, gen(region)
+
+	order idschool idclass idstudent region
+	sort  idschool idclass idstudent
+
+
+	*Keep schools with more than one class
+	gen aux_classes = 1 if idstudent == 1
+	bysort idschool: egen totclasses = sum(aux_classes)
+	keep if totclasses > 1 & totclasses != .
+
+
+	*Ordered class per school
+	bysort idschool: gen idclass2 = sum(aux_classes)
+
+	*Class size
+	bysort idclass: egen class_size = max(idstudent)
+
+
+
+	*Average score of peers
+	bysort idclass: egen peers_score_m = sum(mpuntaje_std)
+	replace peers_score_m = (peers_score_m - mpuntaje_std) / (class_size-1)
+	gen lpeers_score_m = log(peers_score_m)
+
+	bysort idclass: egen peers_score_l = sum(lpuntaje_std)
+	replace peers_score_l = (peers_score_l - lpuntaje_std) / (class_size-1)
+	gen lpeers_score_l = log(peers_score_l)
+
+
+
+	*Average characteristics of peers
+	local varlist = "women edu_madre edu_padre trabaja anios_jardin repitio apoyo"
+	local varlist = "`varlist' buena_relacion auh"
+
+	foreach var of local varlist {
+		bysort idclass: egen peer_`var' = sum(`var')
+		replace peer_`var' = (peer_`var' - `var') / (class_size-1)
+	}
+
+
+
+	*Keep varlist of interest
+	keep idschool idclass idclass2 idstudent region privado urbano nbi  ///
+		 mpuntaje_std log_mpuntaje lpuntaje_std log_lpuntaje   			///	
+	 	 class_size women edu_madre edu_padre trabaja anios_jardin  	///
+	 	 repitio apoyo buena_relacion auh            					///
+	 	 peers_score_l lpeers_score_l peers_score_m lpeers_score_m peer_*
+
+	order idschool idclass idclass2 idstudent region privado urbano nbi  ///
+		  mpuntaje_std log_mpuntaje lpuntaje_std log_lpuntaje   		 ///	
+	 	  class_size women edu_madre edu_padre trabaja anios_jardin  	 ///
+	 	  repitio apoyo buena_relacion auh            					 ///
+	 	  peers_score_l lpeers_score_l peers_score_m lpeers_score_m peer_*
+
+
+
+	save "`data'/analytical/base_`eachbase'_2016.dta", replace
+
+
 }
 
-
-
-*Keep varlist of interest
-keep idschool idclass idclass2 idstudent region privado urbano nbi  ///
-	 mpuntaje_std log_mpuntaje lpuntaje_std log_lpuntaje   			///	
- 	 class_size women edu_madre edu_padre trabaja anios_jardin  	///
- 	 repitio apoyo buena_relacion auh            					///
- 	 peers_score_l lpeers_score_l peers_score_m lpeers_score_m peer_*
-
-order idschool idclass idclass2 idstudent region privado urbano nbi  ///
-	  mpuntaje_std log_mpuntaje lpuntaje_std log_lpuntaje   		 ///	
- 	  class_size women edu_madre edu_padre trabaja anios_jardin  	 ///
- 	  repitio apoyo buena_relacion auh            					 ///
- 	  peers_score_l lpeers_score_l peers_score_m lpeers_score_m peer_*
-
-
-
-save "`data'/analytical/base_6grado_2016.dta", replace
